9fd89459217bb0aa090c324908e5be42ac35aff1
Javier-003 <javiertoledo455@gmail.com>
2024-04-22 09:59:09 -0600

    update urls.py

--- Estadia/urls.py
+++ Estadia/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
-from myappAdmin import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('Home/', views.inicio),
 ]
